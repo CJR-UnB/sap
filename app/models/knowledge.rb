5c50a2ddf3ae88aa1802445f6b4f2375ccdf588c
renanreboredor3 <renanreboredo@paramag.com.br>
2016-08-16 00:31:41 -0300

    Iniciando alteração de tabela de conhecimento

--- app/models/knowledge.rb
+++ app/models/knowledge.rb
@@ -1,12 +1,12 @@
 class Knowledge < ActiveRecord::Base
-  
+
   belongs_to :knowledge_level
   belongs_to :technology
 
   has_and_belongs_to_many :members
 
   def conhecimento_completo
-    "#{self.description}: #{self.knowledge_level.try(:description)} (#{self.technology.try(:name)})"
+    "#{self.name}: #{self.knowledge_level.try(:name)} (#{self.technology.try(:name)})"
   end
 
 end
